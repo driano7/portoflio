5baf0fc057c64b5de6f04c16b9bb9b6b9de9890b
Donovan Riaño <donovanriano@gmail.com>
2025-07-23 01:55:30 +0000

    pon un logo de rayo como favicon en. lugar de firebase

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,9 @@ const inter = Inter({
 export const metadata = {
   title: 'Donovan Riaño | Portfolio',
   description: "Donovan Riaño's personal portfolio website.",
+  icons: {
+    icon: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⚡️</text></svg>',
+  },
 };
 
 export default function RootLayout({
